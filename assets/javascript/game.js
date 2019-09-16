9bfefcf90a5cfb1e64a478d3c7de8dfa7d50575a
the 14th web <dennis.itua1@gmail.com>
2019-09-15 23:13:25 -0400

    updating some new fuctions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,120 +33,168 @@ var userCount = 0;
 var test = 10;
 
 //Randomly choose a choice from the options targetscore, and also displays the result. This is the computer's guess.
+
 targetScore = Math.floor(Math.random() * 101) +19;
-console.log(targetScore); 
+// console.log(targetScore); 
 
 //Query that hold references to the place in the HTML where we want to display things.
+
 $("#number-to-guess").text(' ' + targetScore);
 $("#wins-text").text('Wins: ' + wins);
 $("#losses-text").text('Losses: ' + losses);
 $("#usercount").text(' ' + userCount);
 
 //Randomly choose a choice from the options variable crystal. This is also the computer's guess. 
+   
     var cryst1 = Math.floor(Math.random() * (12 - 1) + 1);
     var cryst2 = Math.floor(Math.random() * (12 - 1) + 1);
     var cryst3 = Math.floor(Math.random() * (12 - 1) + 1);
     var cryst4 = Math.floor(Math.random() * (12 - 1) + 1);
-    console.log(cryst1, cryst2, cryst3, cryst4);
+    // console.log(cryst1, cryst2, cryst3, cryst4);
 
 //Assigns the radomly chose numbers by the computer to each crystal, of different classes.
+
 $(".crystal1").attr("value", cryst1);
 var test = $(".crystal1").attr("value");
-console.log("crystal1 " + test); 
+// console.log("crystal1 " + test); 
 
 $(".crystal2").attr("value", cryst2);
 var test = $(".crystal2").attr("value");
-console.log("crystal2 " + test); 
+// console.log("crystal2 " + test); 
 
 $(".crystal3").attr("value", cryst3);
 var test = $(".crystal3").attr("value");
-console.log("crystal3 " + test); 
+// console.log("crystal3 " + test); 
 
 $(".crystal4").attr("value", cryst4);
 var test = $(".crystal4").attr("value");
-console.log("crystal4 " + test); 
+// console.log("crystal4 " + test); 
 
 //This logic determines the outcome of the game (wins/losses/usercount/reset), increasing and reducing sores and usercount appropriately.
+
 $(".crystal1").on("click", function()
 {
     var test1 = $(this).attr("value");
 
     userCount = parseInt(userCount) + parseInt(test1);
-    console.log("crystal* " + test1);
-    console.log("usercount*** " + userCount);
+    // console.log("crystal* " + test1);
+    // console.log("usercount*** " + userCount);
     $("#usercount").html(userCount);
      
 if (userCount === targetScore) {
     wins++;
     $("#wins-text").text('Wins: ' + wins);
-
+     reset();
  }else if (userCount >= targetScore) {
      losses++;
      $("#losses-text").text('Losses: ' + losses);
-   
+     reset();
  }
 
 });
+
+//This logic determines the outcome of the game (wins/losses/usercount/reset), increasing and reducing sores and usercount appropriately.
 
 $(".crystal2").on("click", function()
 {
     var test1 = $(this).attr("value");
 
     userCount = parseInt(userCount) + parseInt(test1);
-    console.log("crystal* " + test1);
-    console.log("usercount*** " + userCount);
+    // console.log("crystal* " + test1);
+    // console.log("usercount*** " + userCount);
     $("#usercount").html(userCount);
 
     if (userCount === targetScore) {
         wins++;
         $("#wins-text").text('Wins: ' + wins);
-    
+        reset();
      }else if (userCount >= targetScore) {
          losses++;
          $("#losses-text").text('Losses: ' + losses);
-       
+         reset();
      }
 });
+
+//This logic determines the outcome of the game (wins/losses/usercount/reset), increasing and reducing sores and usercount appropriately.
 
 $(".crystal3").on("click", function()
 {
     var test1 = $(this).attr("value");
 
     userCount = parseInt(userCount) + parseInt(test1);
-    console.log("crystal* " + test1);
-    console.log("usercount*** " + userCount);
+    // console.log("crystal* " + test1);
+    // console.log("usercount*** " + userCount);
     $("#usercount").html(userCount);
 
     if (userCount === targetScore) {
         wins++;
         $("#wins-text").text('Wins: ' + wins);
-    
+        reset();
      }else if (userCount >= targetScore) {
          losses++;
          $("#losses-text").text('Losses: ' + losses);
-       
+         reset();  
      }
 });
+
+//This logic determines the outcome of the game (wins/losses/usercount/reset), increasing and reducing sores and usercount appropriately.
 
 $(".crystal4").on("click", function()
 {
     var test1 = $(this).attr("value");
 
     userCount = parseInt(userCount) + parseInt(test1);
-    console.log("crystal* " + test1);
-    console.log("usercount*** " + userCount);
+    // console.log("crystal* " + test1);
+    // console.log("usercount*** " + userCount);
     $("#usercount").html(userCount);
 
     if (userCount === targetScore) {
         wins++;
         $("#wins-text").text('Wins: ' + wins);
-    
+        reset();
      }else if (userCount >= targetScore) {
          losses++;
          $("#losses-text").text('Losses: ' + losses);
-       
+         reset();
      }
 });
+
+//This function will help reset the game when there is a win or loss.
+// reset
+
+function reset()
+{
+    targetScore = Math.floor(Math.random() * 101) +19;
+    targetScore = Math.floor(Math.random() * 100) +20;
+    $("#number-to-guess").html(targetScore);
+
+    // console.log(targetScore);
+
+var cryst1 = Math.floor(Math.random() * (12 - 1) + 1);
+var cryst2 = Math.floor(Math.random() * (12 - 1) + 1);
+var cryst3 = Math.floor(Math.random() * (12 - 1) + 1);
+var cryst4 = Math.floor(Math.random() * (12 - 1) + 1);
+userCount = 0;
+
+$("#usercount").html(userCount);
+
+$(".crystal1").attr("value", cryst1);
+var test = $(".crystal1").attr("value");
+// console.log("crystal1 " + test); 
+
+$(".crystal2").attr("value", cryst2);
+var test = $(".crystal2").attr("value");
+// console.log("crystal2 " + test); 
+
+$(".crystal3").attr("value", cryst3);
+var test = $(".crystal3").attr("value");
+// console.log("crystal3 " + test); 
+
+$(".crystal4").attr("value", cryst4);
+var test = $(".crystal4").attr("value");
+// console.log("crystal4 " + test); 
+
+};
 
 
 
